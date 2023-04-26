a4d370d862ceb9b0799deb3b3d102a44a4f64b56
liempv <liempv@vnext.vn>
2023-04-26 13:14:40 +0700

    Destination Circle2

--- DestinationCircle2/index.js
+++ DestinationCircle2/index.js
@@ -4,67 +4,127 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const colors = ["#F1F6F9", "#394867", "#212A3E", "#9BA4B5", "#3C486B", "#F0F0F0", "#F9D949", "#F45050"];
+
+const mouse = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+}
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    init();;
+})
+
+const getRandomColor = () => {
+    const index = Math.floor(Math.random() * colors.length);
+
+    return colors[index]
+}
+
 const destination = (x1, y1, rad1, x2, y2, rad2) => {
     return rad1 + rad2 >= Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 }
 
 function rotate(velocity, angle) {
-    const rotatedVelocities = {
-      x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-      y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+    const rotateVelocity = {
+        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
     }
-  
-    return rotatedVelocities;
-  }
 
-function resolveCollision(particel, otherPartical) {
-    const xVelocityDiff = particel.velocity.x - otherPartical.velocity.x;
-    const yVelocityDiff = particel.velocity.y - otherPartical.velocity.y;
-  
-    const xDist = otherPartical.x - particel.x;
-    const yDist = otherPartical.y - particel.y;
-  
-    if(xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-      const angle = -Math.atan2(otherPartical.y - particel.y, otherPartical.x - particel.x);
-   
-      const m1 = particel.mass;
-      const m2 = otherPartical.mass;
-  
-      const u1 = rotate(particel.velocity, angle);
-      const u2 = rotate(otherPartical.velocity, angle);
-  
-      const v1 = {x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y};
-      const v2 = {x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y};
-  
-      const vFinal1 = rotate(v1, -angle);
-      const vFinal2 = rotate(v2, -angle);
-  
-      particel.velocity.x =vFinal1.x;
-      particel.velocity.y = vFinal1.y;
-  
-      otherPartical.velocity.x = vFinal2.x;
-      otherPartical.velocity.y = vFinal2.y;
-     }
+    return rotateVelocity;
 }
 
-function Circle(x, y, dx, dy, radius, color) {
+function resolveCollision(praticel, otherPraticel) {
+    const xVelocityDiff = praticel.velocity.x - otherPraticel.velocity.x;
+    const yVelocityDiff = praticel.velocity.y - otherPraticel.velocity.y;
+
+    const xDist = otherPraticel.x - praticel.x;
+    const yDist = otherPraticel.y - praticel.y;
+
+    if(xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+        const angle = Math.atan2(yDist, xDist);
+
+        const m1 = praticel.mass;
+        const m2 = otherPraticel.mass;
+
+        const u1 = rotate(praticel.velocity, -angle);
+        const u2 = rotate(otherPraticel.velocity, -angle);
+
+        const v1 = {x: (((m1 - m2) * u1.x) +2 * m2 * u2.x / (m1 + m2)), y: u1.y};
+        const v2 = {x: ((2 * m1 * u1.x + (m2 - m1) * u2.x)  / (m1 + m2)), y: u2.y};
+
+        const v1Final = rotate(v1, angle);
+        const v2Final = rotate(v2, angle);
+
+        praticel.velocity.x =v1Final.x;
+        praticel.velocity.y = v1Final.y;
+
+        otherPraticel.velocity.x = v2Final.x;
+        otherPraticel.velocity.y = v2Final.y;
+    }
+}
+
+function Circle(x, y, radius, color) {
     this.x = x;
     this.y = y;
-    this.dx = dx;
-    this.dy = dy;
     this.radius = radius;
     this.color = color;
+    this.mass = 1;
+    this.opacity = 0;
+    this.velocity = {
+        x: (Math.random() - 0.5) * 5,
+        y: (Math.random() - 0.5) * 5,
+    }
 
     this.draw = function() {
         ctx.beginPath();
-        ctx.strokeStyle = this.color;
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.save();
+        ctx.globalAlpha = this.opacity;
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.restore();
+        ctx.strokeStyle = this.color; 
         ctx.stroke()
     }
 
     this.update = function(arrCircle) {
+        for(let i = 0; i < arrCircle.length; i++) {
+            if(this === arrCircle[i]) continue;
 
-        this.draw(arrCircle);
+            if(destination(this.x, this.y, this.radius, arrCircle[i].x, arrCircle[i].y, arrCircle[i].radius)) {
+                resolveCollision(this, arrCircle[i])
+            }
+        }
+
+        if(this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.velocity.x = -this.velocity.x;
+        }
+
+        if(this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.velocity.y = -this.velocity.y;
+        }
+
+        if(destination(mouse.x, mouse.y, 60, this.x, this.y, 60) && this.opacity < 1) {
+            this.opacity += 0.02;
+        } else if(this.opacity > 0) {
+            this.opacity -= 0.02;
+
+            this.opacity = Math.max(0, this.opacity)
+        }
+
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+
+        this.draw();
     }
 }
 
@@ -77,32 +137,25 @@ let arrCircle;
 const init = () => {
     arrCircle = [];
 
-    arrCircle.push(new Circle(500, 500, 2, 2, 100, 'red'))
-    arrCircle.push(new Circle(600, 600, 2, 2, 100, 'red'))
+    for(let i = 0; i < 200; i++) {
+        let x = randomNumber(0, canvas.width);
+        let y = randomNumber(0, canvas.height);
+        let rad = 20;
+        const color = getRandomColor();
 
-    // for(let i = 0; i < 10; i++) {
-    //     let x = randomNumber(0, canvas.width);
-    //     let y = randomNumber(0, canvas.height);
-    //     let dx = randomNumber(0, 3);
-    //     let dy = randomNumber(0, 3);
-    //     let rad = randomNumber(30, 50);
+        for(let j = 0; j < arrCircle.length; j++) {
+           if(i === 0) break;
 
-    //     for(let j = 0; j < arrCircle.length; j++) {
-    //        if(i === 0) break;
+            if(destination(x, y, rad, arrCircle[j].x, arrCircle[j].y, arrCircle[j].radius)) {
+                x = randomNumber(0, canvas.width);
+                y = randomNumber(0, canvas.height);
 
-    //         if(destination(x, y, rad, arrCircle[j].x, arrCircle[j].y, arrCircle[j].radius)) {
-    //             x = randomNumber(0, canvas.width);
-    //             y = randomNumber(0, canvas.height);
-    //             dx = randomNumber(0, 3);
-    //             dy = randomNumber(0, 3);
-    //             rad = randomNumber(30, 50);
-
-    //             j = -1;
-    //         }
-    //     }
+                j = -1;
+            }
+        }
         
-    //     arrCircle.push(new Circle(x, y, dx, dy, rad, 'red'))
-    // }
+        arrCircle.push(new Circle(x, y, rad, color))
+    }
 }
 
 const animate = () => {
@@ -110,12 +163,8 @@ const animate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
    
     for(let i = 0; i < arrCircle.length; i++) {
-        arrCircle[i].update()
+        arrCircle[i].update(arrCircle)
     }
-
-    // if(arrCircle.length == 2) {
-    //     resolveCollision(arrCircle[0], arrCircle[1])
-    // }
     
 }
 
